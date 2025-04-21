31a0278709ba3d15b0a5ef1e42f8d7c9f4894a3f
ZhouMo <zzmawa946866@outlook.com>
2025-04-21 17:33:56 +0800

    Changes to be committed: 	modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/utils/logger.rs

--- src/utils/logger.rs
+++ src/utils/logger.rs
@@ -1,16 +1,42 @@
-use std::collections::VecDeque;
-use std::io::{BufWriter, prelude::*};
+use std::{fs::File, io::{self, BufWriter}, path::Path, sync::{mpsc::Sender, mpsc}, thread::{spawn, JoinHandle}};
 
-#[derive(Debug, Clone)]
+use log::Level;
+
+#[derive(Clone, Debug)]
+pub(crate) enum LoggingCommand {
+    Log(Level,String),
+    Exit
+}
+
 pub struct Logger {
-    buf: VecDeque<String>,
-    file: 
+    sender:Sender<LoggingCommand>,
+    handle:JoinHandle<()>
 }
 
 impl Logger {
-    pub fn new() -> Self {
-        Self {
-            buf: VecDeque::new()
-        }
+    pub fn new<P:AsRef<Path>>(log_file:P) -> io::Result<Self> {
+        let (sender,receiver) = mpsc::channel();
+        let io = BufWriter::new(File::create_new(log_file)?);
+        let handle = spawn(move || {
+            let (mut receiver,mut io) = (receiver,io);
+            loop {
+                if let Ok(recv) = receiver.recv() {
+                    match recv {
+                        LoggingCommand::Log(level, text) => {
+                            todo!()
+                        },
+                        LoggingCommand::Exit => {
+                            break;
+                        }
+                    }
+                } else {
+                    break;
+                }
+            }
+        });
+        Ok(Self {
+            sender,
+            handle
+        })
     }
 }
